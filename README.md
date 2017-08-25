248a4cd3c4d0883aa429de58bf210eb04aa3c015
Andrew Duberstein <duberstein@uber.com>
2017-08-25 11:39:57 -0700

    ghpages

--- README.md
+++
@@ -1,3 +0,0 @@
-Soundboard that caches data on the client to persist across sessions.
-
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'semantic-ui-css/semantic.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
